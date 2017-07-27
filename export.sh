8a8fa1ec8b9da1dee31b6a800d9953a5fd01f252
ezirmusitua <jferroal@gmail.com>
2017-07-27 23:16:56 +0800

    Update browserify output and shell file
    Took 15 minutes

--- export.sh
+++ export.sh
@@ -2,14 +2,14 @@
 npm install
 echo " = = = = = Browserify Install = = = = = "
 echo "./node_modules/.bin/browserify -r ./lib/index.js:JMUL -o dist/bundle.js"
-./node_modules/.bin/browserify -r ./lib/index.js:JMUL -o dist/bundle.js
+./node_modules/.bin/browserify ./lib/index.js -o dist/bundle.js
 echo " = = = = = Browserify done = = = = = = "
 echo "prepend greasyfork head "
 sed -i.old '1s;^;\// ==UserScript==\
 // @name              JMUL\
 // @name:zh-CN        自用脚本工具库\
 // @namespace         https://greasyfork.org/users/34556\
-// @version           0.1.0\
+// @version           0.1.1\
 // @description       utilities for monkey scripts\
 // @description:zh-CN 工具库\
 // @author            jferroal\
